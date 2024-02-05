f09afd7eba5d9f91009afec82f49c072d32a2933
Praful Kalhapure <78206541+Praful-Kalhapure@users.noreply.github.com>
2024-02-05 19:36:58 +0530

    Update auth_service.dart

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -12,20 +12,16 @@ class AuthService {
       return userCredential.user;
     } on FirebaseAuthException catch (e) {
       print('Firebase Authentication Error: ${e.message}');
-      // Handle specific Firebase authentication exceptions here
-      // You can customize the error handling based on specific exception codes
+     
 
-      // Example: Handle invalid email
       if (e.code == 'invalid-email') {
         print('Invalid email address');
       }
 
-      // Example: Handle wrong password
       if (e.code == 'wrong-password') {
         print('Wrong password');
       }
 
-      // Add more cases as needed
 
       return null;
     } catch (e) {
